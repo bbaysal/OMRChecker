86c7bff64f01ad4d4485317c761f30df8a2632f4
bbaysal <burakbaysal@hotmail.com>
2024-01-11 14:15:03 +0300

    Ozdemir sample has added

--- home.py
+++ home.py
@@ -1,44 +1,21 @@
 from pathlib import Path
 import streamlit as st
-import tkinter as tk
-from tkinter import filedialog
 
 from src.entry import entry_point
 
-header_container= st.container()
-header_container.header(body="OMR Reader", divider="grey")
-
-header_container.write("OMR Checker programına hoşgeldiniz.")
-header_container.write(
+st.header(body="OMR Reader", divider="grey")
+st.write("OMR Checker programına hoşgeldiniz.")
+st.write(
     "Öncelikle template.json dosyasının ve taranmış dosyaların olduğu klasörü seçiniz.."
 )
 
-def select_folder():
-    root = tk.Tk()
-    root.withdraw()
-    folder_path = filedialog.askdirectory(master=root)
-    root.destroy()
-    return folder_path
-
-st.write(
-    "Input"
-)
-
-input_folder_path = st.session_state.get("input_folder_path", None)
-folder_select_button = st.button("Input Klasör Seç")
-if folder_select_button:
-    input_folder_path = select_folder()
-    st.session_state.input_folder_path = input_folder_path
-
-if input_folder_path:
-    st.write("Seçilen klasör:", input_folder_path)
-
-info_container= st.container()
+input_folder_path = st.text_input("Input Klasörünü Belirleyiniz:")
+output_folder_path = st.text_input("Output Klasörünü Belirleyiniz:")
 
 def evaluate_func():
     input_paths = ["inputs"]
     debug = True
-    output_dir = "outputs"
+    output_dir = output_folder_path
     autoAlign = False
     setLayout = False
 
@@ -49,11 +26,7 @@ def evaluate_func():
         "autoAlign": autoAlign,
         "setLayout": setLayout,
     }
-    try:
-        entry_point(Path(input_folder_path), config, info_container)
-        return True
-    except:
-        return False
+    entry_point(Path(input_folder_path), config)
 
 if st.button(label="Değerlendir.", on_click=evaluate_func, type="primary"):
     st.write("Değerlendirme tamamlamıştır.")
